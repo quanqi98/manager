e316cd64ca3e13f0760ed71ec95608a128d55062
quanqi98 <1437576906@qq.com>
2019-07-26 17:24:16 +0800

    manager后台管理项目初始化加login和index页面

---
+++ src/components/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <el-container class="el">
+    <el-header>Header</el-header>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang='less' scoped>
+.el{
+    width: 100%;
+    height: 100%;
+}
+.el-header {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,44 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//导入要用到的插件
+//element ui 插件
+import ElementUi from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUi);
+
+//路由插件
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+//axios插件
+import axios from 'axios';
+Vue.prototype.$axios = axios;
+
+//导入全局样式
+import './assets/base.css';
+
+
+//导入组件
+import login from './components/login.vue';
+import index from './components/index.vue';
+
+//制定路由规则
+const routes = [
+  {path:'/login',component:login},
+  {path:'/index',component:index},
+];
+
+//实例化路由对象
+const router = new VueRouter({
+  routes
+});
+
+
+
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
